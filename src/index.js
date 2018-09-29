fce545474bb81d80833e0666037b72993745ee8d
wangyan6783 <wangyan6783@gmail.com>
2018-09-29 13:12:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }).join("")
   })
 
+
   document.addEventListener("click", event => {
     if (event.target.className === "trophy-img") {
-      // debugger
       if (topPanel.style.display === "none") {
         topPanel.style.display = "block";
         let groupId = event.target.dataset.id;
@@ -34,13 +34,41 @@ document.addEventListener("DOMContentLoaded", function() {
 
     if (event.target.className === "delete-btn") {
       let groupId = event.target.dataset.id;
+      ACappellaGroup.all() = ACappellaGroup.all().filter(group => group.id != groupId)
       let targetGroup = ACappellaGroup.all().find(group => group.id == groupId);
       let detailRow = document.querySelector(`#detail-${groupId}`);
       detailRow.remove();
       Adapter.deleteGroup(groupId)
     }
 
+    if (event.target.id === "college") {
+      let sortedGroup = ACappellaGroup.all().sort((a, b) => {
+        collegeA = a.college.toUpperCase();
+        collegeB = b.college.toUpperCase();
+        if (collegeA < collegeB) {
+          return -1;
+        }
+        if (collegeA > collegeB) {
+          return 1;
+        }
+        return 0;
+      });
+      groupTableBody.innerHTML = sortedGroup.map(group => group.render()).join("")
+    }
+
+    if (event.target.id === "group-name") {
+      let sortedGroup = ACappellaGroup.all().sort((a, b) => {
+        collegeA = a.name.toUpperCase();
+        collegeB = b.name.toUpperCase();
+        if (collegeA < collegeB) {
+          return -1;
+        }
+        if (collegeA > collegeB) {
+          return 1;
+        }
+        return 0;
+      });
+      groupTableBody.innerHTML = sortedGroup.map(group => group.render()).join("")
+    }
   })
-
-
 });
